3580203fd8b3421b93ed8a7bda70a9464cd9b1dc
caioluzf <159257006+caioluzf@users.noreply.github.com>
2024-02-07 12:28:40 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+let numeroSecreto = 5;
+
+let chute = prompt("Digite um número 1 e 10");
+
+if(numerosecreto == chute) {
+alert("voce acertou")
+}else{
+
+    alert("vocÊ errou")
+}if chute < numeroSecreto{
+    alert{"o numero secreto é maior que ", chute}
+    else{
+
+        alert{ o numero sereto é menor que",chute{
+
+        }
+    }
+var chute = prompt ("digite um numero de 1 a 10")
+}
